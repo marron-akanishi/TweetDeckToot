73fe2702a84d525f8a8c8c99aff211a5122890e2
Marron Akanishi <marron.general@gmail.com>
2017-04-21 23:01:05 +0900

    DM対応

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var script = document.createElement('script');
-var patch = "(function(){$('textarea').keydown(function(e){$('.js-in-reply-to.is-hidden').length!=0&&e.ctrlKey&&e.keyCode==13&&$(('.toot')).trigger('click')})})()"
+var patch = "(function(){$('textarea').keydown(function(e){$('.js-in-reply-to.is-hidden').length!=0&&$('.icon-compose-dm.is-hidden').length!=0&&e.ctrlKey&&e.keyCode==13&&$(('.toot')).trigger('click')})})()"
 script.appendChild(document.createTextNode(patch));
 document.body.appendChild(script);
 setTimeout(patch,3000)
